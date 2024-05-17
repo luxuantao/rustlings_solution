75a6767bfb425166485af6d47ad6c74fefd7ae38
陆轩韬 <luxuantao@xiaohongshu.com>
2024-05-17 10:48:00 +0800

    init

---
+++ functions/functions1.rs
@@ -0,0 +1,11 @@
+// functions1.rs
+//
+// Execute `rustlings hint functions1` or use the `hint` watch subcommand for a
+// hint.
+
+fn call_me() {
+}
+
+fn main() {
+    call_me();
+}
---
+++ generics/generics1.rs
@@ -0,0 +1,12 @@
+// generics1.rs
+//
+// This shopping list program isn't compiling! Use your knowledge of generics to
+// fix it.
+//
+// Execute `rustlings hint generics1` or use the `hint` watch subcommand for a
+// hint.
+
+fn main() {
+    let mut shopping_list: Vec<&str> = Vec::new();
+    shopping_list.push("milk");
+}
